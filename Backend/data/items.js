52ef4df3f2cf172b3d7a23b3a0aeed72dbdafc9b
Pranav <yeolepranav13@example.com>
2025-12-17 18:21:58 +0530

    Fix items.json path for Render

--- Backend/data/items.js
+++ Backend/data/items.js
@@ -1,14 +1,35 @@
-const fs = require('node:fs/promises');
+// const fs = require('node:fs/promises');
+
+// async function getStoredItems() {
+//   const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+//   const data = JSON.parse(rawFileContent);
+//   const storedItems = data.items ?? [];
+//   return storedItems;
+// }
+
+// function storeItems(items) {
+//   return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+// }
+
+// exports.getStoredItems = getStoredItems;
+// exports.storeItems = storeItems;
+
+const fs = require("node:fs/promises");
+const path = require("node:path");
+
+const dataFilePath = path.join(__dirname, "items.json");
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile(dataFilePath, "utf-8");
   const data = JSON.parse(rawFileContent);
-  const storedItems = data.items ?? [];
-  return storedItems;
+  return data.items ?? [];
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(
+    dataFilePath,
+    JSON.stringify({ items: items || [] })
+  );
 }
 
 exports.getStoredItems = getStoredItems;
